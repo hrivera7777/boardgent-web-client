02b8e8b00da24089ef281651d9ccff54a2b80144
Hans Rivera <hrivera@unal.edu.co>
2022-06-06 16:44:47 -0500

    created main component to show the serial number of a computer

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,9 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from "./components/HelloWorld.vue";
+import Message from "./components/Message.vue";
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hi there" />
+  <div class="bg-gray-300 h-screen w-screen flex justify-center">
+    <Message />
+  </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Message.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from "vue";
+import { useSocketIO } from "../helpers/socket.io";
+
+defineProps({
+  msg: String,
+});
+
+const { socket } = useSocketIO();
+socket.on("Socket-messages", () => {
+  console.log("Hi hi :)");
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <div
+    class="p-5 shadow-xl shadow-indigo-500/50 rounded-xl mx-auto my-auto w-fit h-96 grid grid-flow-row bg-gradient-to-r from-cyan-500 to-blue-500"
+  >
+    <h2 class="text-white m-auto text-2xl display: inline;">
+      This is a web app that uses websocket
+    </h2>
+    <div class="flex flex-row">
+      <p class="text-lg mr-5 my-auto text-white">Serial number: 112343</p>
+      <img class="h-64 w-60" src="../assets/images/pc.png" alt="pcImage" />
+    </div>
+    <!-- <button class="rounded-lg bg-orange-400" type="button" @click="count++">
+      count is: {{ count }}
+    </button> -->
+  </div>
+</template>
